a5a4437ec6a55d4514c4e9fa673b0e757d96e300
Jose Prando <jose@votecircle.com>
2016-10-22 21:14:05 -0200

    api fix

--- api/src/main/java/com/vanhack/domain/model/defaultaccountancy/DefaultAccountancy.java
+++ api/src/main/java/com/vanhack/domain/model/defaultaccountancy/DefaultAccountancy.java
@@ -2,8 +2,56 @@ package com.vanhack.domain.model.defaultaccountancy;
 
 import com.vanhack.domain.model.user.User;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
 public class DefaultAccountancy {
 
     private Long id;
+    private String name;
+    private BigDecimal value;
     private User user;
+
+    public DefaultAccountancy(String name, BigDecimal value, User user) {
+        this.name = name;
+        this.value = value;
+        this.user = user;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- api/src/main/java/com/vanhack/domain/model/periodicalyaccountancy/PeriodicalAccountancy.java
+++ api/src/main/java/com/vanhack/domain/model/periodicalyaccountancy/PeriodicalAccountancy.java
@@ -1,17 +1,67 @@
 package com.vanhack.domain.model.periodicalyaccountancy;
 
+import com.vanhack.domain.model.defaultaccountancy.DefaultAccountancy;
 import com.vanhack.domain.model.goal.Goal;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Entity
 public class PeriodicalAccountancy {
 
     private Long id;
     private BigDecimal value;
     private LocalDate entryDate;
-
+    private DefaultAccountancy defaultAccountancy;
     private Goal goal;
+
+    public PeriodicalAccountancy(BigDecimal value, LocalDate entryDate, DefaultAccountancy defaultAccountancy, Goal goal) {
+        this.value = value;
+        this.entryDate = entryDate;
+        this.defaultAccountancy = defaultAccountancy;
+        this.goal = goal;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public LocalDate getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(LocalDate entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public DefaultAccountancy getDefaultAccountancy() {
+        return defaultAccountancy;
+    }
+
+    public void setDefaultAccountancy(DefaultAccountancy defaultAccountancy) {
+        this.defaultAccountancy = defaultAccountancy;
+    }
+
+    public Goal getGoal() {
+        return goal;
+    }
+
+    public void setGoal(Goal goal) {
+        this.goal = goal;
+    }
 }
