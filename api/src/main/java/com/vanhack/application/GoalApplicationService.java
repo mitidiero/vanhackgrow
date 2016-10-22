f1b372e0b6af44735e05885d4c596f155e4774b6
ERS <ERS@ERS-ALIEN>
2016-10-22 19:44:51 -0200

    goal update

--- api/src/main/java/com/vanhack/application/GoalApplicationService.java
+++ api/src/main/java/com/vanhack/application/GoalApplicationService.java
@@ -1,35 +1,41 @@
 package com.vanhack.application;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.vanhack.application.dto.GoalDto;
 import com.vanhack.domain.model.goal.Goal;
 import com.vanhack.domain.model.goal.GoalService;
 import com.vanhack.domain.model.user.User;
 import com.vanhack.domain.model.user.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 public class GoalApplicationService {
 
-    private final static Logger log = LoggerFactory.getLogger(GoalApplicationService.class);
+	private final static Logger log = LoggerFactory.getLogger(GoalApplicationService.class);
 
-    @Autowired
-    private GoalService goalService;
+	@Autowired
+	private GoalService goalService;
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    public GoalDto create(String name, BigDecimal got, BigDecimal goal, LocalDate startDate, LocalDate goalDate, String userEmail) {
-        User user = userService.findByEmail(userEmail);
+	public GoalDto create(String name, BigDecimal got, BigDecimal goal, LocalDate startDate, LocalDate goalDate, String userEmail) {
+		User user = userService.findByEmail(userEmail);
+		Goal goalEntity = new Goal(name, got, goal, startDate, goalDate, user);
+		goalService.create(goalEntity);
+		log.info("Created goal with id {}.", goalEntity.getId());
+		return GoalDto.fromGoal(goalEntity);
+	}
 
-        Goal goalEntity = new Goal(name, got, goal, startDate, goalDate, user);
-
-        goalService.create(goalEntity);
-        log.info("Created goal with id {}.", goalEntity.getId());
-
-        return GoalDto.fromGoal(goalEntity);
-    }
+	public GoalDto update(String userEmail, Long goalID, String goalName, BigDecimal gotAmount, BigDecimal goalAmount, LocalDate startDate,
+	  LocalDate goalDate) {
+		// User user = userService.findByEmail(userEmail);
+		Goal updated = goalService.update(goalID, goalName, gotAmount, goalAmount, startDate, goalDate);
+		log.info("Updated goal with id {}.", updated.getId());
+		return GoalDto.fromGoal(updated);
+	}
 }
